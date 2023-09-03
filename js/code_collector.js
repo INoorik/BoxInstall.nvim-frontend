e9a4dbe1b4f24f441a8a554100af6055ad8155e4
INoorik <ajnurabuzarov8@gmail.com>
2023-09-03 13:50:17 +0300

    collect_code function replaced to new script

--- js/code_collector.js
+++ js/code_collector.js
@@ -1,5 +1,3 @@
-console.log("test")
-
 function collect_code()
 {
 	let result = {};
@@ -9,18 +7,12 @@ function collect_code()
 	{
 		result.code[language] = "";
 		let checkboxes = document.getElementsByClassName(`github_copypaste_nvim_${language}`);
-		console.log(checkboxes);
 		for(let checkbox_idx = 0; checkbox_idx < checkboxes.length; checkbox_idx++)
 		{
 			let checkbox = checkboxes[checkbox_idx];
-			console.log(checkbox);
 			if(checkbox.checked)
 				result.code[language] += checkbox.parentNode.textContent + " ";
 		}
 	}
 	return result;
 }
-
-console.log("ok loaded");
-
-console.log(collect_code());
